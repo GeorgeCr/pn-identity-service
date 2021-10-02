7e8fd1247589d926c760cd902a026d0460558e3d
GeorgeCr <georgecraciun1998@gmail.com>
2021-10-02 18:20:17 +0300

    Fix issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = (process.env.PORT as string) || 3000;
 
 app.listen(PORT, "0.0.0.0", () => console.log(`App listening on port ${PORT}`));
