3555f56e19b232d46c0641e7d7c4b242340ac30c
GeorgeCr <georgecraciun1998@gmail.com>
2021-10-02 18:34:21 +0300

    Change port again :D

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`App listening on port ${PORT}`));
